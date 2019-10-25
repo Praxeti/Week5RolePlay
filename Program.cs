be6137387c086ff7a509bc07e60068ba89ade577
ncfcdaniel <ncfcdaniel@outlook.com>
2019-10-25 13:50:02 +0100

    Created program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week5RolePlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
